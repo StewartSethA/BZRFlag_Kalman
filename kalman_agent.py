b5eeaff0ceb1cf53f095db828571b6d213ec1fa3
pygosceles <pygosceles@yahoo.com>
2013-12-03 12:58:50 -0700

    Create kalman_agent.py

---
+++ kalman_agent.py
@@ -0,0 +1,3 @@
+# This agent is stationary (cannot move) but can change its angle to track and shoot at enemy tanks.
+# It implements a Kalman Filter to track and predict the movements of other tanks
+# And also creates plots (via either GNUPlot or OpenGL) of the probability distributions in order to visualize the efficacy of the Kalman Filter.
